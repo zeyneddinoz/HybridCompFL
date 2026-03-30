5e9f75c08ca04e8d8797c59fe6deadc044596396
Farshad Taebi <farshad_taebi@yahoo.com>
2026-03-30 11:38:09 +0100

    Add files via upload

---
+++ raspberry_pi_codes/runClient.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 /path/to/your/file.txt"
+  exit 1
+fi
+
+FILE="./$1"
+
+SERVERS=("client1_ip" "client2_ip" "client3_ip" "client4_ip" "client5_ip")
+#FILE="./client.py"
+DEST_PATH="/home/ubuntu/FL/testFL/FL/$1"
+
+for i in "${!SERVERS[@]}"; do
+  server="${SERVERS[$i]}"
+  echo "Deploying $DEST_PATH  on  $server with arg: $i"
+  ssh -f "USERNAME@$server" "bash -c ' source /Path to Venv Activate && nohup /Python Path $DEST_PATH $i >> /Log Path  2>&1 &'" 
+done
